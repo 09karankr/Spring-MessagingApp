4ab24cd279e2d91cb21a82fc66517f7c754324dc
Karan Kumar <officialkaran39@gmail.com>
2025-02-21 23:25:41 +0530

    [Karan Kumar]Add. Added the feature to use put method and pass the first name as path variable and last name as query parameter

--- SpringLearning/src/main/java/com/spring/SpringLearning/HelloController.java
+++ SpringLearning/src/main/java/com/spring/SpringLearning/HelloController.java
@@ -1,17 +1,15 @@
 package com.spring.SpringLearning;
 
-import com.spring.SpringLearning.UserDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    @PostMapping("/post")
-    public String sayHello(@RequestBody UserDTO user) {
-        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    @PutMapping("/put/{firstName}")
+    public String sayHello(
+            @PathVariable String firstName,
+            @RequestParam String lastName) {
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
     }
 }
