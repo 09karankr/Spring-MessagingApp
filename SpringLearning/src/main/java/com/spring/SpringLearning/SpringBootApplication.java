8fbce954ea1d604957b294b731770b4964ce2326
Karan Kumar <officialkaran39@gmail.com>
2025-02-21 22:14:38 +0530

    [Karan]Add. Added the program for showing hello from bridgelabz on the port 8080

---
+++ SpringLearning/src/main/java/com/spring/SpringLearning/SpringBootApplication.java
@@ -0,0 +1,10 @@
+package com.spring.SpringLearning;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
